db5e0da372f69c1a76b3daf4a72181979968be1b
Madison Scott-Clary <matthew.scott@canonical.com>
2015-04-14 21:29:32 +0200

    Responding to some review comments
    * linkfetcher instead of embedIcons == false
    * minor cleanup

--- iconfetcher_test.go
+++ iconfetcher_test.go
@@ -14,26 +14,47 @@ type IconFetcherSuite struct{}
 
 var _ = gc.Suite(&IconFetcherSuite{})
 
-var (
-	ts        *httptest.Server
-	tsIconUrl func(*charm.Reference) string
-)
+func (s *IconFetcherSuite) TestLinkFetchIcons(c *gc.C) {
+	tests := map[string]string{
+		"~charming-devs/precise/elasticsearch-2": `
+			<svg xlmns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<image width="96" height="96" xlink:href="/~charming-devs/precise/elasticsearch-2.svg" />
+			</svg>`,
+		"~juju-jitsu/precise/charmworld-58": `
+			<svg xlmns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<image width="96" height="96" xlink:href="/~juju-jitsu/precise/charmworld-58.svg" />
+			</svg>`,
+		"precise/mongodb-21": `
+			<svg xlmns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<image width="96" height="96" xlink:href="/precise/mongodb-21.svg" />
+			</svg>`,
+	}
+	iconUrl := func(ref *charm.Reference) string {
+		return "/" + ref.Path() + ".svg"
+	}
+	b, err := charm.ReadBundleData(strings.NewReader(bundle))
+	c.Assert(err, gc.IsNil)
+	err = b.Verify(nil)
+	c.Assert(err, gc.IsNil)
+	fetcher := LinkFetcher{
+		IconURL: iconUrl,
+	}
+	iconMap, err := fetcher.FetchIcons(b)
+	c.Assert(err, gc.IsNil)
+	for charm, link := range tests {
+		assertXMLEqual(c, []byte(iconMap[charm]), []byte(link))
+	}
+}
 
-func (s *IconFetcherSuite) SetUpTest(c *gc.C) {
-	ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (s *IconFetcherSuite) TestHttpFetchIcons(c *gc.C) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "<svg></svg>")
 	}))
+	defer ts.Close()
 
-	tsIconUrl = func(ref *charm.Reference) string {
+	tsIconUrl := func(ref *charm.Reference) string {
 		return ts.URL + "/" + ref.Path() + ".svg"
 	}
-}
-
-func (s *IconFetcherSuite) TearDownTest(c *gc.C) {
-	ts.Close()
-}
-
-func (s *IconFetcherSuite) TestFetchIcons(c *gc.C) {
 	b, err := charm.ReadBundleData(strings.NewReader(bundle))
 	c.Assert(err, gc.IsNil)
 	err = b.Verify(nil)
@@ -49,18 +70,9 @@ func (s *IconFetcherSuite) TestFetchIcons(c *gc.C) {
 		"~juju-jitsu/precise/charmworld-58":      "<svg></svg>\n",
 		"precise/mongodb-21":                     "<svg></svg>\n",
 	})
-}
 
-func (s *IconFetcherSuite) TestFetchIconsConcurrent(c *gc.C) {
-	b, err := charm.ReadBundleData(strings.NewReader(bundle))
-	c.Assert(err, gc.IsNil)
-	err = b.Verify(nil)
-	c.Assert(err, gc.IsNil)
-	fetcher := HttpFetcher{
-		FetchConcurrently: true,
-		IconURL:           tsIconUrl,
-	}
-	iconMap, err := fetcher.FetchIcons(b)
+	fetcher.FetchConcurrently = true
+	iconMap, err = fetcher.FetchIcons(b)
 	c.Assert(err, gc.IsNil)
 	c.Assert(iconMap, gc.DeepEquals, map[string]string{
 		"~charming-devs/precise/elasticsearch-2": "<svg></svg>\n",
@@ -69,15 +81,14 @@ func (s *IconFetcherSuite) TestFetchIconsConcurrent(c *gc.C) {
 	})
 }
 
-func (s *IconFetcherSuite) TestBadIconURL(c *gc.C) {
-	ts.Close()
-	ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (s *IconFetcherSuite) TestHttpBadIconURL(c *gc.C) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "bad-wolf", http.StatusForbidden)
 		return
 	}))
 	defer ts.Close()
 
-	tsIconUrl = func(ref *charm.Reference) string {
+	tsIconUrl := func(ref *charm.Reference) string {
 		return ts.URL + "/" + ref.Path() + ".svg"
 	}
 
@@ -90,31 +101,11 @@ func (s *IconFetcherSuite) TestBadIconURL(c *gc.C) {
 		IconURL:           tsIconUrl,
 	}
 	iconMap, err := fetcher.FetchIcons(b)
-	c.Assert(err, gc.ErrorMatches, "Error retrieving icon from "+ts.URL+"/precise/mongodb-21.svg: 403 Forbidden")
+	c.Assert(err, gc.ErrorMatches, fmt.Sprintf("Error retrieving icon from %s.+\\.svg: 403 Forbidden", ts.URL))
 	c.Assert(iconMap, gc.IsNil)
-}
 
-func (s *IconFetcherSuite) TestBadIconURLConcurrent(c *gc.C) {
-	ts.Close()
-	ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "bad-wolf", http.StatusForbidden)
-		return
-	}))
-	defer ts.Close()
-
-	tsIconUrl = func(ref *charm.Reference) string {
-		return ts.URL + "/" + ref.Path() + ".svg"
-	}
-
-	b, err := charm.ReadBundleData(strings.NewReader(bundle))
-	c.Assert(err, gc.IsNil)
-	err = b.Verify(nil)
-	c.Assert(err, gc.IsNil)
-	fetcher := HttpFetcher{
-		FetchConcurrently: true,
-		IconURL:           tsIconUrl,
-	}
-	iconMap, err := fetcher.FetchIcons(b)
+	fetcher.FetchConcurrently = true
+	iconMap, err = fetcher.FetchIcons(b)
 	c.Assert(err, gc.ErrorMatches, fmt.Sprintf("Error retrieving icon from %s.+\\.svg: 403 Forbidden", ts.URL))
 	c.Assert(iconMap, gc.IsNil)
 }
