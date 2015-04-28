83c1382389c4fb3287cfd276f7152686ac234ef3
Madison Scott-Clary <matthew.scott@canonical.com>
2015-04-28 11:33:53 -0600

    move setting id to a separate func

--- svg.go
+++ svg.go
@@ -2,7 +2,6 @@ package jujusvg
 
 import (
 	"io"
-	"strings"
 
 	"github.com/juju/xml"
 	"gopkg.in/errgo.v1"
@@ -13,7 +12,10 @@ const svgNamespace = "http://www.w3.org/2000/svg"
 // Process an icon SVG file from a reader, removing anything surrounding
 // the <svg></svg> tags, which would be invalid in this context (such as
 // <?xml...?> decls, directives, etc), writing out to a writer.  In
-// addition, loosely check that the icon is a valid SVG file.
+// addition, loosely check that the icon is a valid SVG file.  The id
+// argument provides a unique identifier for the icon SVG so that it can
+// be referenced within the bundle diagram.  If an id attribute on the SVG
+// tag already exists, it will be replaced with this argument.
 func processIcon(r io.Reader, w io.Writer, id string) error {
 	dec := xml.NewDecoder(r)
 	dec.DefaultSpace = svgNamespace
@@ -35,21 +37,11 @@ func processIcon(r io.Reader, w io.Writer, id string) error {
 		if ok && tag.Name.Space == svgNamespace && tag.Name.Local == "svg" {
 			svgStartFound = true
 			depth++
-			st := tok.(xml.StartElement)
-			for i := range st.Attr {
-				if strings.ToLower(st.Attr[i].Name.Local) == "id" {
-					st.Attr = append(st.Attr[:i], st.Attr[i+1:]...)
-					break
-				}
-			}
-			st.Attr = append(st.Attr, xml.Attr{
-				Name: xml.Name{
-					Local: "id",
-				},
-				Value: id,
-			})
-			if err := enc.EncodeToken(st); err != nil {
-				return errgo.Notef(err, "cannot encode token %#v", st)
+			tag.Attr = setXMLAttr(tag.Attr, xml.Name{
+				Local: "id",
+			}, id)
+			if err := enc.EncodeToken(tag); err != nil {
+				return errgo.Notef(err, "cannot encode token %#v", tag)
 			}
 		}
 	}
@@ -88,4 +80,24 @@ func processIcon(r io.Reader, w io.Writer, id string) error {
 	}
 
 	return nil
+}
+
+// setAttr returns the given attributes with the given attribute name set to
+// val, adding an attribute if necessary.
+func setXMLAttr(attrs []xml.Attr, name xml.Name, val string) []xml.Attr {
+	found := false
+	for i := range attrs {
+		if attrs[i].Name == name {
+			attrs[i].Value = val
+			found = true
+			break
+		}
+	}
+	if !found {
+		attrs = append(attrs, xml.Attr{
+			Name:  name,
+			Value: val,
+		})
+	}
+	return attrs
 }
